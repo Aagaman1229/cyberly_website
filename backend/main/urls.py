a3e3fd22083af38458dde4d3bbb4151168b8dd79
Aagaman1229 <aagaman4321@gmail.com>
2025-06-22 06:50:37 +0545

    connected the forntend and backend

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import article_list, article_detail
 
 urlpatterns = [
-    path('article/', article_list, name='article-list'),
-    path('article/<slug:slug>/', article_detail, name='article-detail'),
+    path('', article_list, name='article-list'),
+    path('<slug:slug>/', article_detail, name='article-detail'),
 ]
